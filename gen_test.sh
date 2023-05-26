21e2832983da03b166b1e2584074cb9974f11ae5
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-05-26 15:44:20 +0000

    debug: save

---
+++ gen_test.sh
@@ -0,0 +1,9 @@
+
+
+PKG_CONFIG_PATH=/opt/rh/gcc-toolset-12/root/usr/lib64/pkgconfig:/usr/lib64/pkgconfig
+export PKG_CONFIG_PATH
+
+#./cargo nightly test --workspace --lib -- tests --nocapture > out
+
+grep test_case_json out | sed -e 's/.*test_case_json//' -e 's/$/,/' | sort > out.json
+
