89409f8743581d736dda188a0c673077a0b2095a
Josh Siegel <83408952+jumpsiegel@users.noreply.github.com>
2023-06-02 15:21:55 +0000

    debug-14: more hacks

--- gen_test.sh
+++ gen_test.sh
@@ -3,9 +3,10 @@
 PKG_CONFIG_PATH=/opt/rh/gcc-toolset-12/root/usr/lib64/pkgconfig:/usr/lib64/pkgconfig
 export PKG_CONFIG_PATH
 
-./cargo nightly test --package solana-runtime --lib -- system_instruction_processor::tests --nocapture > out
+./cargo nightly test --package solana-runtime --lib -- system_instruction_processor::tests::test_process_withdraw_ix_no_acc_data_fail --nocapture > out
+#./cargo nightly test --package solana-runtime --lib -- system_instruction_processor::tests::test_create_from_account_is_nonce_fail --nocapture > out
 #./cargo nightly test --workspace --lib -- tests --nocapture > out
 
-grep test_case_json out | sed -e 's/.*test_case_json//' -e 's/$/,/' | sort > out.json
+#grep test_case_json out | sed -e 's/.*test_case_json//' -e 's/$/,/' | sort > out.json
 
 # grep test_transfer_lamports out.json
