45a178607004267be4280466a40b8a75fd2f5643
Michael McGee <mmcgee@jumptrading.com>
2023-10-06 21:02:13 +0000

    firedancer: build validator binaries as libraries
    We want to link the validator and genesis binaries into our Firedancer
    binary for zero configuration and easy deployment. We also don't want
    to rename the main.rs files, since that would cause repeated merge
    conflicts and make it hard to tell what Firedancer has changed. To do
    this, we point lib.rs at main.rs (main is a module), and then don't try
    build the binary.

--- genesis/src/lib.rs
+++ genesis/src/lib.rs
@@ -14,3 +14,30 @@ pub struct Base64Account {
     pub data: String,
     pub executable: bool,
 }
+
+/// FIREDANCER: Kind of hacky but we do this to make the change as surgical as
+/// possible so we don't keep generating merge conflicts. Main is treated as
+/// a module that's imported by the library.
+mod main;
+
+/// FIREDANCER: Firedancer links directly to the Solana Labs client so that it can
+/// build and distribute one binary. This exported function is what it calls to
+/// start up the Solana Labs child process side.
+#[no_mangle]
+pub extern "C" fn solana_genesis_main(argv: *const *const i8) {
+    use std::os::unix::ffi::OsStringExt;
+    use std::ffi::{CStr, OsString};
+
+    let mut args = vec![];
+
+    let mut index = 0;
+    unsafe {
+        while !(*argv.offset(index)).is_null() {
+            args.push(OsString::from_vec(CStr::from_ptr(*argv.offset(index)).to_bytes().to_vec()));
+
+            index += 1;
+        }
+    }
+
+    main::main(args.into_iter().map(OsString::from)).unwrap();
+}
